0ec492aefb30f226e63cdca6212516ff0a7df86a
morg <morgan.guimard@gmail.com>
2021-08-31 10:24:07 +0200

    Use QColorDialog

--- ColorPicker.cpp
+++ ColorPicker.cpp
@@ -7,6 +7,7 @@
 #include <QFile>
 #include <QDialog>
 #include <QVBoxLayout>
+#include <QColorDialog>
 #include "ColorUtils.h"
 
 ColorPicker::ColorPicker(QWidget *parent) :
@@ -34,52 +35,13 @@ void ColorPicker::SetRGBColor(RGBColor color)
 
 void ColorPicker::on_button_clicked()
 {    
-    QPoint button_pos = ui->button->cursor().pos();
+    QColorDialog *colorPicker = new QColorDialog(this);
+    colorPicker->setAttribute(Qt::WA_DeleteOnClose);
 
-    QDialog* dialog = new QDialog();
+    connect(colorPicker, &QColorDialog::colorSelected,[=](const QColor &color){
+               SetQColor(color);
+               emit ColorSelected(color);
+    });
 
-    if (OpenRGBEffectsPlugin::DarkTheme)
-    {
-        QPalette pal;
-        pal.setColor(QPalette::WindowText, Qt::white);
-        dialog->setPalette(pal);
-        QFile dark_theme(":/windows_dark.qss");
-        dark_theme.open(QFile::ReadOnly);
-        dialog->setStyleSheet(dark_theme.readAll());
-        dark_theme.close();
-    }
-
-    dialog->setMinimumSize(300,320);
-    dialog->setModal(true);
-
-    QVBoxLayout* dialog_layout = new QVBoxLayout(dialog);
-
-    ColorWheel* color_wheel = new ColorWheel(dialog);
-
-    dialog_layout->addWidget(color_wheel);
-
-    QHBoxLayout* buttons_layout = new QHBoxLayout();
-
-    QPushButton* accept_button = new QPushButton();
-    accept_button->setText("Set Color");
-    dialog->connect(accept_button,SIGNAL(clicked()),dialog,SLOT(accept()));
-    buttons_layout->addWidget(accept_button);
-
-    QPushButton* cancel_button = new QPushButton();
-    cancel_button->setText("Cancel");
-    dialog->connect(cancel_button,SIGNAL(clicked()),dialog,SLOT(reject()));
-    buttons_layout->addWidget(cancel_button);
-
-    dialog_layout->addLayout(buttons_layout);
-
-    dialog->move(button_pos.x(), button_pos.y());
-
-    if (dialog->exec())
-    {
-        QColor color = color_wheel->color();
-        SetQColor(color);
-
-        emit ColorSelected(color);
-        delete dialog;
-    }
+    colorPicker->open();
 }
