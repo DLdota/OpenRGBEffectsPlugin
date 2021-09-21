8aa95fa36a9e4bd0912ab351a2a11e6f4fac255c
morg <morgan.guimard@gmail.com>
2021-09-21 11:46:38 +0200

    New audio shader

---
+++ shaders/audio-example-4.fs
@@ -0,0 +1,57 @@
+#define c vec2(0.5,0.5)
+#define gain 100.
+#define PI 3.14159
+
+vec4 HSVToRGB(float h, float s, float v)
+    {
+    float min;
+    float chroma;
+    float hPrime;
+    float x;
+    vec4 rgbColor;
+
+    chroma = s * v;
+    hPrime = h / 60.0;
+    x = chroma * (1.0 - abs((mod(hPrime, 2.0) - 1.0)));
+
+    if(hPrime < 1.0) {
+            rgbColor.r = chroma;
+            rgbColor.g = x;
+    } else if(hPrime < 2.0) {
+            rgbColor.r = x;
+            rgbColor.g = chroma;
+    } else if(hPrime < 3.0) {
+            rgbColor.g = chroma;
+            rgbColor.b = x;
+    } else if(hPrime < 4.0) {
+            rgbColor.g = x;
+            rgbColor.b = chroma;
+    } else if(hPrime < 5.0) {
+            rgbColor.r = x;
+            rgbColor.b = chroma;
+    } else if(hPrime <= 6.0) {
+            rgbColor.r = chroma;
+            rgbColor.b = x;
+    }
+
+    min = v - chroma;
+
+    rgbColor.r += min;
+    rgbColor.g += min;
+    rgbColor.b += min;
+    rgbColor.a  = 1.0;
+
+    return rgbColor;
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 p = abs(fragCoord.xy / iResolution.xy - 0.5);
+    float v = clamp(gain*(0.5-p.y) * iAudio[floor((p.x)*256.)], 0.0, 1.0);
+
+    float h = 360.0 * mod(p.x+iTime, PI) / PI;
+    float s = 1.0;
+
+    fragColor = HSVToRGB(h, s, v);
+}
+
