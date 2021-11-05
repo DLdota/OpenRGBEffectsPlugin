cfcbe2e73e420bea9ad35c280254d7bfe80fdc66
morg <morgan.guimard@gmail.com>
2021-11-05 10:39:06 +0100

    Missing line in a shader

--- shaders/audio-example-10.fs
+++ shaders/audio-example-10.fs
@@ -2,6 +2,7 @@
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
+    vec2 uv = fragCoord.xy / iResolution.xy;
     vec2 center = vec2(0.5);
     float value = iAudio[int(abs(uv.x-center.x)*256.)] * iAudio[int(abs(uv.y-center.y)*256.)];
     fragColor = value * vec4(c, 1.);
